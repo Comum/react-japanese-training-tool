a2ad3d4a01cd8a0e7dacfc85de453241ff9707f0
Comum <miguel.rib.20@gmail.com>
2019-04-25 11:39:32 +0100

    change home-container to use class

--- src/scenes/home-container.js
+++ src/scenes/home-container.js
@@ -3,10 +3,24 @@ import styled from 'styled-components';
 
 import MainModal from './components/main-modal';
 
-const HomeContainer = () => (
-	<MainModal>
-		<div>Home container</div>
-	</MainModal>
-);
+class HomeContainer extends React.Component {
+	state = {
+		tabs: [
+			{ id: 1, name: 'Intro', selected: true },
+			{ id: 2, name: 'Hiragana', selected: false },
+			{ id: 3, name: 'Katakana', selected: false },
+			{ id: 4, name: 'Alphabet practice', selected: false },
+			{ id: 4, name: 'Rules', selected: false },
+		],
+	};
+
+	render() {
+		return (
+			<MainModal>
+				<div>Home container</div>
+			</MainModal>
+		);
+	}
+}
 
 export default HomeContainer;
