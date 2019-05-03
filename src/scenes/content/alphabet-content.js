e1df7881893fe441b127f93f8db15192efed6cd8
Comum <miguel.rib.20@gmail.com>
2019-05-03 20:49:54 +0100

    add title to alphabet content page

--- src/scenes/content/alphabet-content.js
+++ src/scenes/content/alphabet-content.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const AlphabetContent = () => <div>Alphabet content</div>
+const MainContainer = styled.div`
+    width: 100%;
+    height: 100%;
+
+    overflow-y: auto;
+`;
+
+const TitleContainer = styled.div`
+    width: 100%;
+    height: 50px;
+
+    display: flex;
+    flex-direction: row;
+`;
+
+const Cell = styled.div`
+    flex-grow: 1;
+    flex-shrink: 0;
+    flex-basis: 0;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`;
+
+const AlphabetContent = () => <MainContainer>
+    <TitleContainer>
+        <Cell>Romaji</Cell>
+        <Cell>Hiragana</Cell>
+        <Cell>Katakana</Cell>
+    </TitleContainer>
+</MainContainer>
 
 export default AlphabetContent;
